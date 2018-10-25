af6f08ebbaddf7d2fab3dfcc0f7bebd140f081f1
DaMatrix <joey.rabil@gmail.com>
2018-10-25 18:07:57 +0200

    common library

--- minecraft/src/main/java/net/daporkchop/lib/minecraft/world/format/SaveFormat.java
+++ minecraft/src/main/java/net/daporkchop/lib/minecraft/world/format/SaveFormat.java
@@ -12,22 +12,11 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-rootProject.name = 'root'
 
-include 'binary'
-include 'crypto'
-include 'common'
-include 'db'
-include 'encoding'
-include 'hash'
-include 'http'
-include 'math'
-include 'minecraft'
-include 'minecraft:minecraft-worldscanner'
-include 'nbt'
-include 'network'
-include 'noise'
-include 'primitive'
-include 'primitive:generator'
+package net.daporkchop.lib.minecraft.world.format;
 
-findProject(':minecraft:minecraft-worldscanner')?.name = 'minecraft-worldscanner'
+/**
+ * @author DaPorkchop_
+ */
+public interface SaveFormat {
+}

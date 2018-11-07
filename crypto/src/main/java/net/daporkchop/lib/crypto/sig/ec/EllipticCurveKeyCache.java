e03ccf09cad761d27075054567d3769452f9c506
DaMatrix <joey.rabil@gmail.com>
2018-11-07 13:44:41 +0100

    commit a thing that i need to use
    (cherry picked from commit cfdba95)

---
+++ crypto/src/main/java/net/daporkchop/lib/crypto/sig/ec/EllipticCurveKeyCache.java
@@ -0,0 +1,83 @@
+/*
+ * Adapted from the Wizardry License
+ *
+ * Copyright (c) 2018-2018 DaPorkchop_ and contributors
+ *
+ * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
+ *
+ * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from DaPorkchop_.
+ *
+ * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.lib.crypto.sig.ec;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import net.daporkchop.lib.crypto.key.EllipticCurveKeyPair;
+import net.daporkchop.lib.crypto.keygen.KeyGen;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @author DaPorkchop_
+ */
+<<<<<<<HEAD:network/src/main/java/net/daporkchop/lib/network/conn/Session.java
+//TODO
+@Setter(AccessLevel.PACKAGE)
+public abstract class Session {
+    @Setter(AccessLevel.PACKAGE)
+    private Connection connection;
+
+    public void send(@NonNull Packet packet) {
+    }
+
+    public int getPing() {
+        return -1;
+    }
+
+    public boolean isConnected() {
+        return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <E extends Endpoint> E getEndpoint() {
+        return null;
+    }
+
+    public InetSocketAddress getRemoteAddress() {
+        return null;
+=======
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EllipticCurveKeyCache {
+    private static final Map<CurveType, CompletableFuture<EllipticCurveKeyPair>> cache = new EnumMap<>(CurveType.class);
+
+    public static CompletableFuture<EllipticCurveKeyPair> getFutureKeyPair(@NonNull CurveType curveType)    {
+        synchronized (cache)    {
+            if (cache.containsKey(curveType))   {
+                return cache.get(curveType);
+            } else {
+                CompletableFuture<EllipticCurveKeyPair> future = new CompletableFuture<>();
+                cache.put(curveType, future);
+                ForkJoinPool.commonPool().submit(() -> future.complete(KeyGen.gen(curveType)));
+                return future;
+            }
+        }
+    }
+
+    public static EllipticCurveKeyPair getKeyPair(@NonNull CurveType type)  {
+        try {
+            return getFutureKeyPair(type).get();
+        } catch (Exception e)   {
+            throw new RuntimeException(e);
+        }
+>>>>>>> 810b6d7... nuke networking completely, and add ECDH util to crypto:crypto/src/main/java/net/daporkchop/lib/crypto/sig/ec/EllipticCurveKeyCache.java
+    }
+}

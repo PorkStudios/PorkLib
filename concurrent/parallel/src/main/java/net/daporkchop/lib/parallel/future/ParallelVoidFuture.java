62118a5a7b5a0ac9d01490b4cd45fbba201ec333
DaMatrix <joey.rabil@gmail.com>
2018-12-05 13:35:20 +0100

    start work on some parallel things!

--- concurrent/parallel/src/main/java/net/daporkchop/lib/parallel/future/ParallelVoidFuture.java
+++ concurrent/parallel/src/main/java/net/daporkchop/lib/parallel/future/ParallelVoidFuture.java
@@ -13,44 +13,26 @@
  *
  */
 
-package net.daporkchop.lib.network.pork.packet;
+package net.daporkchop.lib.parallel.future;
 
-import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NonNull;
-import net.daporkchop.lib.binary.stream.DataIn;
-import net.daporkchop.lib.binary.stream.DataOut;
-import net.daporkchop.lib.network.channel.Channel;
-import net.daporkchop.lib.network.endpoint.client.PorkClient;
-import net.daporkchop.lib.network.packet.Codec;
-import net.daporkchop.lib.network.packet.Packet;
-import net.daporkchop.lib.network.pork.PorkConnection;
-
-import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import net.daporkchop.lib.concurrent.future.VoidFuture;
+import net.daporkchop.lib.parallel.protocol.ParallelConnection;
 
 /**
  * @author DaPorkchop_
  */
-@NoArgsConstructor
-public class HandshakeCompletePacket implements Packet {
-    @Override
-    public void read(DataIn in) throws IOException {
-        //in.readBytesSimple();
+@AllArgsConstructor
+@Getter
+public class ParallelVoidFuture extends VoidFuture implements ParallelFuture {
+    protected long taskId;
+
+    protected ParallelVoidFuture()  {
     }
 
-    @Override
-    public void write(DataOut out) throws IOException {
-        //out.writeBytesSimple(new byte[0xFFFFFF]);
-    }
-
-    public static class HandshakeCompleteCodec implements Codec<HandshakeCompletePacket, PorkConnection> {
-        @Override
-        public void handle(@NonNull HandshakeCompletePacket packet, @NonNull Channel channel, @NonNull PorkConnection connection) {
-            connection.<PorkClient>getEndpoint().postConnectCallback(null);
-        }
-
-        @Override
-        public HandshakeCompletePacket createInstance() {
-            return new HandshakeCompletePacket();
-        }
-    }
+    @NonNull
+    private ParallelConnection connection;
 }

b8a51079e7ab586c650ccd17b54f77d160fd280a
DaMatrix <joey.rabil@gmail.com>
2019-02-08 00:35:01 +0100

    temp commit

--- gui/src/main/java/net/daporkchop/lib/gui/component/type/Window.java
+++ gui/src/main/java/net/daporkchop/lib/gui/component/type/Window.java
@@ -13,31 +13,16 @@
  *
  */
 
-package gui;
+package net.daporkchop.lib.gui.component.type;
 
-import net.daporkchop.lib.gui.component.type.Button;
-import net.daporkchop.lib.gui.component.type.Window;
+import lombok.NonNull;
+import net.daporkchop.lib.gui.component.Container;
 
 /**
  * @author DaPorkchop_
  */
-public class GuiExample {
-    public static void main(String... args) throws InterruptedException {
-        /*Window window = GuiSystem.swing().newWindow(0, 0, 512, 256);
-        window.setTitle("Example GUI").show();
-        Thread.sleep(1000L);
-        Button b = window.addButton("button1")
-                .setClickHandler(() -> System.out.println("Clicked!"))
-                .setText("Example Button")
-                .setTooltip("Check out my cool button!")
-                .setUpdater(ComponentUpdater.of(40.0d, 45.0d, 20.0d, 10.0d));*/
-        Window window = null;
-        Button button = null;
+public interface Window extends Container<Window> {
 
-        button = button.onClick();
-        button = button.setName("");
 
-        window = window.setTitle("");
-        window = window.setName("");
-    }
+    Window setTitle(@NonNull String title);
 }

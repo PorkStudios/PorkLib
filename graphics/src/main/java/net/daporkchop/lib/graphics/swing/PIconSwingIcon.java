4ee4bc44aad91ec965c782be1c66f06823d58985
DaMatrix <joey.rabil@gmail.com>
2019-02-24 17:12:27 +0100

    render things as icons (slowly)

--- graphics/src/main/java/net/daporkchop/lib/graphics/swing/PIconSwingIcon.java
+++ graphics/src/main/java/net/daporkchop/lib/graphics/swing/PIconSwingIcon.java
@@ -13,7 +13,7 @@
  *
  */
 
-package net.daporkchop.lib.graphics.impl;
+package net.daporkchop.lib.graphics.swing;
 
 import lombok.Getter;
 import lombok.NonNull;
@@ -21,39 +21,37 @@ import lombok.RequiredArgsConstructor;
 import net.daporkchop.lib.graphics.PIcon;
 
 import javax.swing.*;
-import java.awt.image.BufferedImage;
+import java.awt.*;
 
 /**
+ * A very, very inefficient implementation of {@link Icon} that uses a {@link PIcon} to store pixel values.
+ *
  * @author DaPorkchop_
  */
 @RequiredArgsConstructor
 @Getter
-public class PIconBufferedImage implements PIcon {
+public class PIconSwingIcon implements Icon {
     @NonNull
-    protected final BufferedImage delegate;
+    protected final PIcon delegate;
 
     @Override
-    public int getWidth() {
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        for (int xx = this.delegate.getWidth() - 1; xx >= 0; xx--)  {
+            for (int yy = this.delegate.getHeight() - 1; yy >= 0; yy--) {
+                g.setPaintMode();
+                g.setColor(new Color(this.delegate.getARGB(xx, yy)));
+                g.drawRect(x + xx, y + yy, 1, 1);
+            }
+        }
+    }
+
+    @Override
+    public int getIconWidth() {
         return this.delegate.getWidth();
     }
 
     @Override
-    public int getHeight() {
+    public int getIconHeight() {
         return this.delegate.getHeight();
-    }
-
-    @Override
-    public boolean isBW() {
-        return false;
-    }
-
-    @Override
-    public int getARGB(int x, int y) {
-        return this.delegate.getRGB(x, y);
-    }
-
-    @Override
-    public Icon getAsSwingIcon() {
-        return new ImageIcon(this.delegate);
     }
 }

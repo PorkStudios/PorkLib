3206fddfad53060161c2356962604cd35c168d48
DaMatrix <joey.rabil@gmail.com>
2019-04-09 09:26:18 +0200

    start trying to add tables... this'll be a big task... :/

---
+++ gui/src/main/java/net/daporkchop/lib/gui/component/type/functional/Table.java
@@ -0,0 +1,45 @@
+/*
+ * Adapted from the Wizardry License
+ *
+ * Copyright (c) 2018-2019 DaPorkchop_ and contributors
+ *
+ * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
+ *
+ * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from DaPorkchop_.
+ *
+ * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.lib.gui.component.type.functional;
+
+import lombok.NonNull;
+import net.daporkchop.lib.gui.component.Component;
+import net.daporkchop.lib.gui.component.state.functional.TableState;
+
+/**
+ * @author DaPorkchop_
+ */
+public interface Table extends Component<Table, TableState> {
+    @Override
+    default TableState getState() {
+        return this.isVisible() ?
+                this.isEnabled() ?
+                        this.isHovered() ? TableState.ENABLED_HOVERED : TableState.ENABLED
+                        : this.isHovered() ? TableState.DISABLED_HOVERED : TableState.DISABLED
+                : TableState.HIDDEN;
+    }
+
+    int getColumns();
+    int getRows();
+
+    String getColumnName(int col);
+    Table setColumnName(int col, @NonNull String name);
+
+    Table removeColumn(int col);
+    Table removeRow(int row);
+
+    Table addColumn(String name, @NonNull Class<?> clazz);
+}

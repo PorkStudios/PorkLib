f62768da1be9bd2b27ffef5eb61c5150c022a7d3
DaMatrix <joey.rabil@gmail.com>
2019-04-11 23:26:44 +0200

    move some more unsafe stuff to unsafe module
    was taking a break from this, hopefully gonna be able to get this done with soon(tm)

--- math/src/main/java/net/daporkchop/lib/math/arrays/grid/impl/direct/DirectIntGrid2d.java
+++ math/src/main/java/net/daporkchop/lib/math/arrays/grid/impl/direct/DirectIntGrid2d.java
@@ -15,7 +15,7 @@
 
 package net.daporkchop.lib.math.arrays.grid.impl.direct;
 
-import net.daporkchop.lib.common.util.DirectMemoryHolder;
+import net.daporkchop.lib.unsafe.capability.DirectMemoryHolder;
 import net.daporkchop.lib.unsafe.PUnsafe;
 import net.daporkchop.lib.math.arrays.grid.Grid2d;
 

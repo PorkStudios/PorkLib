9b7fc3f0f665eaf542804bf360bce67a20ee978e
DaMatrix <joey.rabil@gmail.com>
2019-04-23 19:32:01 +0200

    add a simple implementation of a scheduler that uses a Java timer as a delegate

---
+++ concurrent/src/main/java/net/daporkchop/lib/concurrent/tasks/impl/TimerTaskScheduler.java
@@ -0,0 +1,115 @@
+/*
+ * Adapted from the Wizardry License
+ *
+ * Copyright (c) 2018-2019 DaPorkchop_ and contributors
+ *
+ * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
+ *
+ * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from DaPorkchop_.
+ *
+ * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.lib.concurrent.tasks.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import net.daporkchop.lib.concurrent.future.Future;
+import net.daporkchop.lib.concurrent.future.ReturnableFuture;
+import net.daporkchop.lib.concurrent.future.impl.SimpleFuture;
+import net.daporkchop.lib.concurrent.future.impl.SimpleReturnableFuture;
+import net.daporkchop.lib.concurrent.tasks.TaskScheduler;
+import net.daporkchop.lib.math.primitive.PMath;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
+
+/**
+ * @author DaPorkchop_
+ */
+@AllArgsConstructor
+public class TimerTaskScheduler implements TaskScheduler {
+    protected final Object mutex = new Object[0];
+
+    @NonNull
+    protected Timer timer;
+
+    public TimerTaskScheduler() {
+        this(new Timer());
+    }
+
+    @Override
+    public Future schedule(@NonNull Runnable task, long time) {
+        synchronized (this.mutex) {
+            Future future = new SimpleFuture();
+            this.timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        task.run();
+                        future.complete();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        future.completeExceptionally(e);
+                    }
+                }
+            }, PMath.max(System.currentTimeMillis() - time, 0L));
+            return future;
+        }
+    }
+
+    @Override
+    public <T> ReturnableFuture<T> schedule(@NonNull Supplier<T> task, long time) {
+        synchronized (this.mutex) {
+            ReturnableFuture<T> future = new SimpleReturnableFuture<>();
+            this.timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        future.complete(task.get());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        future.completeExceptionally(e);
+                    }
+                }
+            }, PMath.max(System.currentTimeMillis() - time, 0L));
+            return future;
+        }
+    }
+
+    @Override
+    public void scheduleRepeating(@NonNull BooleanSupplier task, long startTime, long interval) {
+        synchronized (this.mutex) {
+            this.timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (!task.getAsBoolean()) {
+                        this.cancel();
+                    }
+                }
+            }, PMath.max(System.currentTimeMillis() - startTime, 0L), interval);
+        }
+    }
+
+    @Override
+    public void close() {
+        synchronized (this.mutex) {
+            this.timer = null;
+        }
+    }
+
+    @Override
+    public void cancel() {
+        synchronized (this.mutex) {
+            this.timer.cancel();
+            this.timer.purge();
+            this.timer = null;
+        }
+    }
+}

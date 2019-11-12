eae6b2054106f6d378c69967d7bc431591601d22
DaMatrix <joey.rabil@gmail.com>
2019-11-12 13:10:08 +0100

    start making a new api

---
+++ http/src/main/java/net/daporkchop/lib/http/HttpClient.java
@@ -0,0 +1,38 @@
+/*
+ * Adapted from the Wizardry License
+ *
+ * Copyright (c) 2018-2019 DaPorkchop_ and contributors
+ *
+ * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
+ *
+ * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from DaPorkchop_.
+ *
+ * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.lib.http;
+
+import io.netty.util.concurrent.Future;
+
+/**
+ * A representation of an HTTP client.
+ *
+ * @author DaPorkchop_
+ */
+public interface HttpClient {
+    /**
+     * Closes this {@link HttpClient}, disconnecting any active requests and releasing any resources allocated
+     * by it.
+     *
+     * @return a {@link Future} which will be notified when the close operation has been completed
+     */
+    Future<Void> close();
+
+    /**
+     * @return a {@link Future} which will be notified when this {@link HttpClient} instance has been closed
+     */
+    Future<Void> closeFuture();
+}
---
+++ http/src/main/java/net/daporkchop/lib/http/request/RequestBuilder.java
@@ -0,0 +1,45 @@
+/*
+ * Adapted from the Wizardry License
+ *
+ * Copyright (c) 2018-2019 DaPorkchop_ and contributors
+ *
+ * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
+ *
+ * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from DaPorkchop_.
+ *
+ * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.lib.http.request;
+
+import lombok.NonNull;
+
+/**
+ * Used for configuring an HTTP request prior to sending.
+ * <p>
+ * Assume that all implementations will cause undefined behavior when used concurrently.
+ *
+ * @author DaPorkchop_
+ */
+public interface RequestBuilder<V, R extends Request<V>> {
+    /**
+     * Configures this {@link RequestBuilder} to send the request to the given URL.
+     *
+     * @param url the URL that the request should be sent to
+     * @return this {@link RequestBuilder} instance
+     */
+    RequestBuilder<V, R> configure(@NonNull String url);
+
+    /**
+     * Initiates the HTTP request using the configured settings.
+     * <p>
+     * Once this method has been called, this {@link RequestBuilder} instance should be discarded and any attempts
+     * to use it will result in undefined behavior.
+     *
+     * @return an {@link Request} constructed using the configured settings
+     */
+    R send();
+}

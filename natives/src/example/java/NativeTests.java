22e891b737ead58dbeab04eee499c270b91c89ad
DaMatrix <daporkchop@daporkchop.net>
2019-11-30 17:25:38 +0100

    native module is just about done

--- natives/src/example/java/NativeTests.java
+++ natives/src/example/java/NativeTests.java
@@ -15,15 +15,13 @@
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import net.daporkchop.lib.common.util.PorkUtil;
 import net.daporkchop.lib.natives.PNatives;
 import net.daporkchop.lib.natives.zlib.PDeflater;
 import net.daporkchop.lib.natives.zlib.PInflater;
 import net.daporkchop.lib.natives.zlib.Zlib;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
-import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -33,66 +31,29 @@ public class NativeTests {
     private static final int SIZE = 67108864; //64 MiB
 
     public static void main(String... args) throws IOException {
-        if (true) {
-            ByteBuf orig = Unpooled.directBuffer(SIZE).writerIndex(SIZE);
-            for (int i = 0; i < SIZE; i++) {
-                orig.setByte(i, ThreadLocalRandom.current().nextInt(8));
-            }
-            ByteBuf compressed = Unpooled.directBuffer(SIZE >>> 3, SIZE).clear();
+        ByteBuf orig = Unpooled.directBuffer(SIZE).writerIndex(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            orig.setByte(i, ThreadLocalRandom.current().nextInt(8));
+        }
+        ByteBuf compressed = Unpooled.directBuffer(SIZE >>> 3, SIZE).clear();
 
-            try (PDeflater deflater =
-                         PNatives.ZLIB.get().deflater(Zlib.ZLIB_LEVEL_BEST)
-                 //new net.daporkchop.lib.natives.zlib.JavaDeflater(Zlib.ZLIB_LEVEL_BEST, Zlib.ZLIB_MODE_ZLIB)
-            ) {
-                System.out.printf("Deflating...\nFinished: %b\n", deflater.finished());
-                deflater.deflate(orig, compressed);
-                System.out.printf("Read: %d\nWritten: %d\nFinished: %b\n", deflater.readBytes(), deflater.writtenBytes(), deflater.finished());
-            }
+        try (PDeflater deflater = PNatives.ZLIB.get().deflater(Zlib.ZLIB_LEVEL_BEST)) {
+            System.out.printf("Deflating with %s...\nFinished: %b\n", PorkUtil.className(deflater), deflater.finished());
+            deflater.deflate(orig, compressed);
+            System.out.printf("Read: %d\nWritten: %d\nFinished: %b\n", deflater.readBytes(), deflater.writtenBytes(), deflater.finished());
+        }
 
-            ByteBuf decompressed = Unpooled.directBuffer(SIZE, SIZE).clear();
+        ByteBuf decompressed = Unpooled.directBuffer(SIZE, SIZE).clear();
 
-            try (PInflater inflater =
-                         PNatives.ZLIB.get().inflater()
-                 //new net.daporkchop.lib.natives.zlib.JavaInflater(Zlib.ZLIB_MODE_ZLIB)
-            ) {
-                System.out.printf("Inflating...\nFinished: %b\n", inflater.finished());
-                inflater.inflate(compressed, decompressed);
-                System.out.printf("Read: %d\nWritten: %d\nFinished: %b\n", inflater.readBytes(), inflater.writtenBytes(), inflater.finished());
-            }
+        try (PInflater inflater = PNatives.ZLIB.get().inflater()) {
+            System.out.printf("Inflating with %s...\nFinished: %b\n", PorkUtil.className(inflater), inflater.finished());
+            inflater.inflate(compressed, decompressed);
+            System.out.printf("Read: %d\nWritten: %d\nFinished: %b\n", inflater.readBytes(), inflater.writtenBytes(), inflater.finished());
+        }
 
-            for (int i = 0; i < SIZE; i++) {
-                if (orig.getByte(i) != decompressed.getByte(i)) {
-                    throw new IllegalStateException();
-                }
-            }
-        } else if (true) {
-            File srcFile = new File("/home/daporkchop/Desktop/java.tar.gz");
-            File dstFile = new File("/home/daporkchop/Desktop/java.tar");
-
-            ByteBuf src;
-            try (FileChannel channel = FileChannel.open(srcFile.toPath(), StandardOpenOption.READ)) {
-                int size = (int) channel.size();
-                src = Unpooled.directBuffer(size, size).clear();
-                src.writeBytes(channel, 0L, size);
-                if (src.isWritable()) {
-                    throw new IllegalStateException(String.format("Only read %d bytes!", src.writerIndex()));
-                }
-            }
-
-            System.out.printf("Original size: %d bytes\n", src.readableBytes());
-
-            ByteBuf dst = Unpooled.directBuffer(src.readableBytes(), SIZE);
-            try (PInflater inflater = PNatives.ZLIB.get().inflater(Zlib.ZLIB_MODE_AUTO)) {
-                inflater.inflate(src, dst);
-            }
-
-            System.out.printf("Inflated size: %d bytes\n", dst.readableBytes());
-
-            try (FileChannel channel = FileChannel.open(dstFile.toPath(), StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-                dst.readBytes(channel, 0L, dst.readableBytes());
-                if (dst.isReadable()) {
-                    throw new IllegalStateException(String.format("Only wrote %d bytes!", src.readableBytes()));
-                }
+        for (int i = 0; i < SIZE; i++) {
+            if (orig.getByte(i) != decompressed.getByte(i)) {
+                throw new IllegalStateException();
             }
         }
     }

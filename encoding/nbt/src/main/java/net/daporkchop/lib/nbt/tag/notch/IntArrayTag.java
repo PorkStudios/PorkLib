8525cd78abb34accc1fc6a140de6220523028a98
DaMatrix <daporkchop@daporkchop.net>
2020-01-11 19:50:13 +0100

    add a way to encode NBT data in a streaming fashion

--- encoding/nbt/src/main/java/net/daporkchop/lib/nbt/tag/notch/IntArrayTag.java
+++ encoding/nbt/src/main/java/net/daporkchop/lib/nbt/tag/notch/IntArrayTag.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from the Wizardry License
  *
- * Copyright (c) 2018-2019 DaPorkchop_ and contributors
+ * Copyright (c) 2018-2020 DaPorkchop_ and contributors
  *
  * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
  *
@@ -51,18 +51,21 @@ public class IntArrayTag extends Tag {
 
     @Override
     public synchronized void read(NBTInputStream in, TagRegistry registry) throws IOException {
-        int length = in.readInt();
+        final int length = in.readInt();
         this.value(in.alloc().intArray(length));
+        final int[] value = this.value;
         for (int i = 0; i < length; i++) {
-            this.value[i] = in.readInt();
+            value[i] = in.readInt();
         }
     }
 
     @Override
     public void write(NBTOutputStream out, TagRegistry registry) throws IOException {
-        out.writeInt(this.value.length);
-        for (int i = 0; i < this.value.length; i++) {
-            out.writeInt(this.value[i]);
+        final int[] value = this.value;
+        final int length = value.length;
+        out.writeInt(length);
+        for (int i = 0; i < length; i++) {
+            out.writeInt(value[i]);
         }
     }
 

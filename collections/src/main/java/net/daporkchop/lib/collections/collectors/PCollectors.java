6e11c8aecdb86d19dcdf257e98af88b3d9f270ee
DaPorkchop_ <daporkchop@daporkchop.net>
2024-12-28 15:19:00 +0100

    common: add set and list collectors to PCollectors

--- collections/src/main/java/net/daporkchop/lib/collections/collectors/PCollectors.java
+++ collections/src/main/java/net/daporkchop/lib/collections/collectors/PCollectors.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from The MIT License (MIT)
  *
- * Copyright (c) 2018-2020 DaPorkchop_
+ * Copyright (c) 2018-2024 DaPorkchop_
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -15,15 +15,23 @@
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 package net.daporkchop.lib.collections.collectors;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -37,6 +45,10 @@ import java.util.stream.Collectors;
  */
 @UtilityClass
 public class PCollectors {
+    //
+    // Map utility lambdas
+    //
+
     /**
      * Public version of {@link Collectors#throwingMerger()}.
      *
@@ -49,21 +61,181 @@ public class PCollectors {
     }
 
     /**
-     * Variant of {@link Collectors#toMap(Function, Function)} which allows defining a custom {@link Map} supplier.
-     *
-     * @see Collectors#toMap(Function, Function)
+     * @return a {@link Function} which returns the key of a {@link Map.Entry map entry}
      */
-    public static <T, K, U> Collector<T, ?, Map<K, U>> toMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper, Supplier<Map<K, U>> mapSupplier) {
-        return Collectors.toMap(keyMapper, valueMapper, throwingMerger(), mapSupplier);
+    public static <K> Function<? extends Map.Entry<K, ?>, ? super K> mapEntryKey() {
+        return Map.Entry::getKey;
     }
 
     /**
-     * Variant of {@link Collectors#toMap(Function, Function)} which uses a {@link LinkedHashMap} rather than a {@link java.util.HashMap} to
-     * preserve entry order.
+     * @return a {@link Function} which returns the value of a {@link Map.Entry map entry}
+     */
+    public static <V> Function<? extends Map.Entry<?, V>, ? super V> mapEntryValue() {
+        return Map.Entry::getValue;
+    }
+
+    //
+    // Collection factory lambdas
+    //
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link HashMap}
+     */
+    public static <K, V> Supplier<? super HashMap<K, V>> hashMapFactory() {
+        return HashMap::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link LinkedHashMap}
+     */
+    public static <K, V> Supplier<? super LinkedHashMap<K, V>> linkedHashMapFactory() {
+        return LinkedHashMap::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link TreeMap}
+     */
+    public static <K, V> Supplier<? super TreeMap<K, V>> treeMapFactory() {
+        return TreeMap::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link HashSet}
+     */
+    public static <E> Supplier<? super HashSet<E>> hashSetFactory() {
+        return HashSet::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link LinkedHashSet}
+     */
+    public static <E> Supplier<? super LinkedHashSet<E>> linkedHashSetFactory() {
+        return LinkedHashSet::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link TreeSet}
+     */
+    public static <E> Supplier<? super TreeSet<E>> treeSetFactory() {
+        return TreeSet::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link ArrayList}
+     */
+    public static <E> Supplier<? super ArrayList<E>> arrayListFactory() {
+        return ArrayList::new;
+    }
+
+    /**
+     * @return a {@link Supplier} which returns new instances of {@link LinkedList}
+     */
+    public static <E> Supplier<? super LinkedList<E>> linkedListFactory() {
+        return LinkedList::new;
+    }
+
+    //
+    // Collectors
+    //
+
+    /**
+     * Variant of {@link Collectors#toMap(Function, Function)} which allows using a custom {@link Map} factory.
      *
      * @see Collectors#toMap(Function, Function)
      */
-    public static <T, K, U> Collector<T, ?, Map<K, U>> toLinkedHashMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
-        return Collectors.toMap(keyMapper, valueMapper, throwingMerger(), LinkedHashMap::new);
+    public static <T, K, U, M extends Map<K, U>> Collector<T, ?, M> toMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper, Supplier<M> mapFactory) {
+        return Collectors.toMap(keyMapper, valueMapper, throwingMerger(), mapFactory);
+    }
+
+    /**
+     * Variant of {@link Collectors#toMap(Function, Function)} which always returns a {@link HashMap}.
+     *
+     * @see Collectors#toMap(Function, Function)
+     */
+    public static <T, K, U> Collector<T, ?, HashMap<K, U>> toHashMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
+        return toMap(keyMapper, valueMapper, hashMapFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toMap(Function, Function)} which always returns a {@link LinkedHashMap} with entries added in encounter order.
+     *
+     * @see Collectors#toMap(Function, Function)
+     */
+    public static <T, K, U> Collector<T, ?, LinkedHashMap<K, U>> toLinkedHashMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
+        return toMap(keyMapper, valueMapper, linkedHashMapFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toMap(Function, Function)} which always returns a {@link TreeMap}.
+     *
+     * @see Collectors#toMap(Function, Function)
+     */
+    public static <T, K, U> Collector<T, ?, TreeMap<K, U>> toTreeMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
+        return toMap(keyMapper, valueMapper, treeMapFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toSet()} which allows using a custom {@link Set} factory.
+     *
+     * @param setFactory a {@link Supplier} which provides new instances of the target {@link Set} implementation
+     * @see Collectors#toSet()
+     */
+    public static <E, S extends Set<E>> Collector<E, ?, S> toSet(Supplier<S> setFactory) {
+        return Collectors.toCollection(setFactory);
+    }
+
+    /**
+     * Variant of {@link Collectors#toSet()} which always returns a {@link HashSet}.
+     *
+     * @see Collectors#toSet()
+     */
+    public static <E> Collector<E, ?, HashSet<E>> toHashSet() {
+        return toSet(hashSetFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toSet()} which always returns a {@link LinkedHashSet} with elements added in encounter order.
+     *
+     * @see Collectors#toSet()
+     */
+    public static <E> Collector<E, ?, LinkedHashSet<E>> toLinkedHashSet() {
+        return toSet(linkedHashSetFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toSet()} which always returns a {@link TreeSet}.
+     *
+     * @see Collectors#toSet()
+     */
+    public static <E> Collector<E, ?, TreeSet<E>> toTreeSet() {
+        return toSet(treeSetFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toList()} which allows using a custom {@link List} factory.
+     *
+     * @param listFactory a {@link Supplier} which provides new instances of the target {@link List} implementation
+     * @see Collectors#toList()
+     */
+    public static <E, L extends List<E>> Collector<E, ?, L> toList(Supplier<L> listFactory) {
+        return Collectors.toCollection(listFactory);
+    }
+
+    /**
+     * Variant of {@link Collectors#toList()} which always returns an {@link ArrayList}.
+     *
+     * @see Collectors#toList()
+     */
+    public static <E> Collector<E, ?, ArrayList<E>> toArrayList() {
+        return toList(arrayListFactory());
+    }
+
+    /**
+     * Variant of {@link Collectors#toList()} which always returns a {@link LinkedList}.
+     *
+     * @see Collectors#toList()
+     */
+    public static <E> Collector<E, ?, LinkedList<E>> toLinkedList() {
+        return toList(linkedListFactory());
     }
 }

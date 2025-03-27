83dc806b757ef201e2ac32de5dddd0cc8009ef6c
DaPorkchop_ <daporkchop@daporkchop.net>
2025-03-27 15:23:04 +0100

    common: add more functions to PFunctions

--- common/src/main/java/net/daporkchop/lib/common/function/PFunctions.java
+++ common/src/main/java/net/daporkchop/lib/common/function/PFunctions.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from The MIT License (MIT)
  *
- * Copyright (c) 2018-2024 DaPorkchop_
+ * Copyright (c) 2018-2025 DaPorkchop_
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -15,6 +15,7 @@
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  */
 
 package net.daporkchop.lib.common.function;
@@ -24,21 +25,30 @@ import lombok.experimental.UtilityClass;
 import net.daporkchop.lib.common.function.exception.ESupplier;
 
 import java.lang.reflect.Constructor;
+import java.util.Objects;
+import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.DoubleConsumer;
+import java.util.function.DoubleFunction;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
+import java.util.function.IntFunction;
+import java.util.function.IntSupplier;
 import java.util.function.LongConsumer;
+import java.util.function.LongFunction;
 import java.util.function.ObjDoubleConsumer;
 import java.util.function.ObjIntConsumer;
 import java.util.function.ObjLongConsumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 import java.util.function.UnaryOperator;
 
 /**
  * Some useful methods for dealing with (((functions))) i.e. in this case functional interfaces, which in most cases will be lambda expressions.
+ * <p>
+ * For the most part, these exist to reduce the number of lambda metaclasses which need to be generated and loaded for common lambda expressions.
  *
  * @author DaPorkchop_
  */
@@ -155,5 +165,110 @@ public class PFunctions {
      */
     public static <T> ObjDoubleConsumer<T> noopObjDoubleConsumer() {
         return (ignored0, ignored1) -> {};
+    }
+
+    /**
+     * @return a {@link Predicate} which performs no action and returns {@code true}
+     */
+    public static <T> Predicate<T> truePredicate() {
+        return ignored -> true;
+    }
+
+    /**
+     * @return a {@link Predicate} which performs no action and returns {@code false}
+     */
+    public static <T> Predicate<T> falsePredicate() {
+        return ignored -> false;
+    }
+
+    /**
+     * @return a {@link Predicate} which performs no action and returns the given value
+     */
+    public static <T> Predicate<T> constantPredicate(boolean value) {
+        return ignored -> value;
+    }
+
+    /**
+     * @return a {@link Supplier} which performs no action and returns {@code null}
+     */
+    public static <T> Supplier<T> nullSupplier() {
+        return () -> null;
+    }
+
+    /**
+     * @return a {@link Callable} which performs no action and returns {@code null}
+     */
+    public static <T> Callable<T> nullCallable() {
+        return () -> null;
+    }
+
+    /**
+     * @return a {@link Function} which performs no action and returns {@code null}
+     */
+    public static <T, R> Function<T, R> nullFunction() {
+        return ignored -> null;
+    }
+
+    /**
+     * @return a {@link Supplier} which performs no action and returns the given value
+     */
+    public static <T> Supplier<T> constantSupplier(T value) {
+        return () -> value;
+    }
+
+    /**
+     * @return a {@link Callable} which performs no action and returns the given value
+     */
+    public static <T> Callable<T> constantCallable(T value) {
+        return () -> value;
+    }
+
+    /**
+     * @return a {@link Function} which performs no action and returns the given value
+     */
+    public static <T, R> Function<T, R> constantFunction(R value) {
+        return ignored -> value;
+    }
+
+    /**
+     * @return a {@link Predicate} which calls {@link Objects#isNull(Object)}
+     */
+    public static <T> Predicate<T> isNullPredicate() {
+        return Objects::isNull;
+    }
+
+    /**
+     * @return a {@link Predicate} which calls {@link Objects#nonNull(Object)}
+     */
+    public static <T> Predicate<T> nonNullPredicate() {
+        return Objects::nonNull;
+    }
+
+    /**
+     * @return a {@link Function} which calls {@link String#valueOf}
+     */
+    public static <T> Function<T, String> toStringFunction() {
+        return String::valueOf;
+    }
+
+    /**
+     * @return an {@link IntFunction} which calls {@link String#valueOf(int)}
+     */
+    public static IntFunction<String> toStringIntFunction() {
+        return String::valueOf;
+    }
+
+    /**
+     * @return a {@link LongFunction} which calls {@link String#valueOf(long)}
+     */
+    public static LongFunction<String> toStringLongFunction() {
+        return String::valueOf;
+    }
+
+    /**
+     * @return a {@link DoubleFunction} which calls {@link String#valueOf(double)}
+     */
+    public static DoubleFunction<String> toStringDoubleFunction() {
+        return String::valueOf;
     }
 }
